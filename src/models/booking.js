76d2c4741a2091dbc9589569d30e4c5979cd9ba8
AmCongTu <phamminhquan12c1.com>
2023-07-17 00:16:11 +0700

    commit code

--- src/models/booking.js
+++ src/models/booking.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Booking.belongsTo(models.Room, { foreignKey: 'room' });
+
     }
   }
   Booking.init({
--- src/routes/filter.js
+++ src/routes/filter.js
@@ -1,11 +1,13 @@
 import express from "express";
-import {filterRooms} from "../controllers/filter.js"
+import {checkAvailability, filterRooms} from "../controllers/filter.js"
 
 
 const router = express.Router()
 
 
 router.get("/room", filterRooms)
+
+router.get("/room/available", checkAvailability)
 
 
 
