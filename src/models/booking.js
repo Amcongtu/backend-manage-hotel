dff563ecdc7554a3c64657031af2d1daf95d82ad
AmCongTu <phamminhquan12c1.com>
2023-07-17 13:30:57 +0700

    commit code

--- src/models/booking.js
+++ src/models/booking.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       Booking.belongsTo(models.Room, { foreignKey: 'room' });
+      Booking.hasMany(models.ServiceOfBooking, { foreignKey: 'service'});
 
     }
   }
