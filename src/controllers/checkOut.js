ddeaf4a563aa4f7abe1e9a7b6b1f2de20aae66c4
AmCongTu <phamminhquan12c1.com>
2023-07-18 21:29:31 +0700

    commit code

--- src/controllers/checkOut.js
+++ src/controllers/checkOut.js
@@ -4,6 +4,11 @@ import { responseHelper } from "../helpers/response.js"
 export const createCheckOut = async (req, res) => {
     const { booking, date, description, status, employee } = req.body;
 
+    const currentDate = new Date();
+    const checkOutDate = date || currentDate; // Sử dụng date từ request body hoặc lấy ngày giờ hiện tại dưới định dạng ISO
+
+    const transaction = await db.sequelize.transaction();
+
     try {
         const existingBooking = await db.Booking.findOne({ where: { id: booking } });
         const existingEmployee = await db.Employee.findOne({ where: { id: employee } });
@@ -18,14 +23,21 @@ export const createCheckOut = async (req, res) => {
 
         const checkOut = await db.CheckOut.create({
             booking,
-            date,
-            description: description || "", 
+            date: checkOutDate,
+            description: description || "",
             status: status || "checkedOut",
             employee
-        });
+        }, { transaction });
+
+        // Cập nhật trạng thái của booking thành "checkedOut"
+        existingBooking.status = "checkedOut";
+        await existingBooking.save({ transaction });
+
+        await transaction.commit();
 
         return res.status(200).json(responseHelper(200, "Thêm Check-out thành công", true, checkOut));
     } catch (error) {
+        await transaction.rollback();
         console.log(error);
         return res.status(500).json(responseHelper(500, "Thêm Check-out không thành công", false, []));
     }
