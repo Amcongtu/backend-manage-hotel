eaab0e22fecea57f4277d5486a85f4b6791937b9
AmCongTu <phamminhquan12c1.com>
2023-07-19 10:25:18 +0700

    commit code

--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -244,7 +244,15 @@ export const createBooking = async (req, res) => {
         );
 
         await transaction.commit();
-
+        let transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: "phamminhquan12c1@gmail.com", // generated ethereal user
+                pass: process.env.APP_PASS_MAIL // generated ethereal password
+            }
+        })
 
         try {
             await transporter.sendMail(
@@ -352,7 +360,7 @@ export const updateBookingStatus = async (req, res) => {
 
         booking.status = status;
         booking.employee = employee
-        
+
         await booking.save({ transaction });
         let transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
