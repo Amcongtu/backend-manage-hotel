536f6bcabc7d4f24aaaefc9b652dbbe26c5fbdf0
AmCongTu <phamminhquan12c1.com>
2023-07-23 11:41:27 +0700

    commit code

--- src/controllers/checkIn.js
+++ src/controllers/checkIn.js
@@ -2,11 +2,10 @@ import db from "../models/index.js"
 import { responseHelper } from "../helpers/response.js"
 
 export const createCheckIn = async (req, res) => {
-    const { booking, date, status, description, employee } = req.body;
+    const { booking, date, status, description, employee, services } = req.body;
 
-    
     const currentDate = new Date();
-    const checkInDate = date || currentDate; // Sử dụng date từ request body hoặc lấy ngày giờ hiện tại dưới định dạng ISO
+    const checkInDate = date || currentDate;
 
     const transaction = await db.sequelize.transaction();
 
@@ -21,6 +20,34 @@ export const createCheckIn = async (req, res) => {
         if (!existingEmployee) {
             return res.status(400).json(responseHelper(400, "Nhân viên không tồn tại", false, []));
         }
+
+        // Tính tổng tiền dịch vụ sau khi thêm vào
+        let serviceTotal = 0;
+        for (const service of services) {
+            const existingService = await db.Service.findOne({ where: { id: service.id } });
+            if (!existingService) {
+                await transaction.rollback();
+                return res.status(400).json(responseHelper(400, `Dịch vụ với ID ${service.id} không tồn tại`, false, []));
+            }
+
+            // Thêm dịch vụ vào ServiceOfBooking
+            await db.ServiceOfBooking.create(
+                {
+                    booking,
+                    service: service.id,
+                    createdAt: checkInDate,
+                    updatedAt: checkInDate
+                },
+                { transaction }
+            );
+
+            // Cộng tiền dịch vụ vào tổng tiền
+            serviceTotal += existingService.amount;
+        }
+
+        // Cập nhật trường total của bảng Booking
+        existingBooking.total += serviceTotal;
+        await existingBooking.save({ transaction });
 
         const checkIn = await db.CheckIn.create({
             booking,
@@ -39,7 +66,7 @@ export const createCheckIn = async (req, res) => {
         return res.status(200).json(responseHelper(200, "Tạo Check-in thành công", true, checkIn));
     } catch (error) {
         await transaction.rollback();
-        // console.log(error);
+        console.log(error);
         return res.status(500).json(responseHelper(500, "Tạo Check-in không thành công", false, []));
     }
 };
